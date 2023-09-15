8c02e264b73664d42c17c6eb9be7a83dc9635011
Ava Smith <ava013005@gmail.com>
2023-09-15 11:54:21 -0500

    finished view lecture

--- src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -1,10 +1,12 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import edu.codeup.codeupspringblog.model.Item;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/hello")
@@ -23,4 +25,30 @@ public class HelloController {
         return String.format("Hello, %s!", name);
     }
 
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(Model model) {
+        List<Item> shoppingCart = new ArrayList<>();
+        shoppingCart.add(new Item("screwdriver"));
+        shoppingCart.add(new Item("hammer"));
+        shoppingCart.add(new Item("drill"));
+        model.addAttribute("shoppingCart", shoppingCart);
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        List<Item> shoppingCart = new ArrayList<>();
+        shoppingCart.add(new Item("screwdriver"));
+        shoppingCart.add(new Item("hammer"));
+        shoppingCart.add(new Item("drill"));
+        model.addAttribute("shoppingCart", shoppingCart);
+        return "join";
+    }
 }
--- src/main/java/edu/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,16 +1,25 @@
 package edu.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage () {
-        return "This is the landing page!";
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String landingPage () {
+//        return "This is the landing page!";
+//    }
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
+
 
 }
